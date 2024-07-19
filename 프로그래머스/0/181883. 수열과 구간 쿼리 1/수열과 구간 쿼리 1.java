09c97945a37b655798ac908a94f2c5a52eb1a5c3
kang <rkdtjd1020@naver.com>
2024-07-19 16:09:30 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 5.28 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        for (int i = 0; i < queries.length; i++){
+            for (int k = 0; k < arr.length; k++){
+                if(queries[i][0] <= k && k <= queries[i][1]){
+                    arr[k]++;
+                }
+            }
+        }
+        return arr;
+    }
+}
